6362400e645d1cd44b06f32926d2d2eeed1ef899
cecilia <[cecilia.narambuena@ayi-asociados.com]>
2020-09-07 01:35:50 -0300

    resolucion adivinanza demo

--- script.js
+++ script.js
@@ -1,44 +1,119 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     let botonProbar = document.getElementById('boton-probar')
-    let respuestaAdivinanza = document.getElementById('respuesta-adivinanza')
+    let intento = 0
+    let numeroClicksBoton = 0
 
-    botonProbar.addEventListener('click', () => {
+    const resolverAdivinanza = (respuesta) => {
 
-        let valor = respuestaAdivinanza.value
-
-
-        const resolverAdivinanza = (respuesta) => {
-
-            const sinEspacios = (oracion) => {
-                let sinEspaciosFinal = oracion.trim()
-                let sinEspacios = sinEspaciosFinal.split(' ').join('')
-                return sinEspacios
-            }
-
-            let respuestaConfig = sinEspacios(respuesta).toUpperCase()
-
-
-            if (respuestaConfig == 'NACEUNAESTRELLA' || respuestaConfig == 'ASTARTISBORN') {
-
-                respuestaConfig = 'ACERTASTE'
-
-            } else {
-
-                respuestaConfig = 'FALLASTE'
-
-            }
-
-
-            return respuestaConfig
+        const sinEspacios = (oracion) => {
+            let sinEspaciosFinal = oracion.trim()
+            let sinEspacios = sinEspaciosFinal.split(' ').join('')
+            return sinEspacios
         }
 
+        let respuestaConfig = sinEspacios(respuesta).toUpperCase()
+        let resultado
 
 
-        console.log(resolverAdivinanza(valor))
+        if (respuestaConfig == 'NACEUNAESTRELLA' || respuestaConfig == 'ASTARTISBORN') {
+
+            resultado = true
+
+        } else {
+
+            resultado = false
+
+        }
+
+        return resultado
+    }
+
+    /*
+    botonProbar.addEventListener('click', () => {
+
+        let respuestaAdivinanza = document.getElementById('respuesta-adivinanza')
+        let valor = respuestaAdivinanza.value
+
+        numeroClicksBoton += 1
+
+        if (numeroClicksBoton < 5 && resolverAdivinanza(valor)) {
+
+            console.log('lo lograste')
+
+        } else {
+
+            switch (numeroClicksBoton) {
+                case 1:
+                    console.log('Quedan 3 intentos')
+                    resolverAdivinanza(valor)
+                    break;
+                case 2:
+                    console.log('Quedan 2 intentos')
+                    resolverAdivinanza(valor)
+                    break;
+                case 3:
+                    console.log('Último intento')
+                    break;
+                default:
+                    if (resolverAdivinanza(valor)) {
+                        console.log('lo lograste')
+                    } else {
+                        console.log('perdiste')
+                    }
+                    break
+            }
+        }
+
+    })*/
+
+    //
+
+    /*for (intento; intento < 4; intento++) {
+
+        botonProbar.addEventListener('click', () => {
+
+            let respuestaAdivinanza = document.getElementById('respuesta-adivinanza')
+            let valor = respuestaAdivinanza.value
+
+            console.log('intento numero' + intento + ' ' + valor)
+
+            if (resolverAdivinanza(valor)) {
+                intento = 4
+            }
+
+
+        })
+
+    }*/
+
+    const capturarRespuesta = () => {
+        botonProbar.addEventListener('click', (event) => {
+            let respuestaAdivinanza = document.getElementById('respuesta-adivinanza')
+            let respuestaEnviada = respuestaAdivinanza.value
+            event.preventDefault()
+                //console.log(respuestaEnviada)
+            return respuestaEnviada
+        })
+
+    }
+
+    console.log(capturarRespuesta())
+
+    /*for (intento; intento < 4; intento++) {
 
 
 
-    })
+        console.log('intento numero' + intento +)
+
+        if (intento == 0) {
+            let esCorrecta = resolverAdivinanza(valor)
+            if (esCorrecta) {
+                break
+            }
+
+        }
+
+    }*/
 
 })
